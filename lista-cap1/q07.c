c624eb4aea00fb3aa6ae26504b9e8db03b79bf28
Danilo Barros <barrosnilo2@gmail.com>
2026-08-26 14:50:51 -0300

    Create q07.c

---
+++ lista-cap1/q07.c
@@ -0,0 +1,23 @@
+a)printf("\n\tBom dia! Shirley.");
+
+
+    Bom dia! Shirley.
+b)printf("Você já tomou café? \n");
+
+Você já tomou café? 
+
+c) printf("\n\nA solução não existe!\nNão insista.");
+
+
+
+A solução não existe!
+Não insista.
+d)printf("Duas\tlinhas\tde\tsaída\nou\tuma?");
+
+Duas    linhas  de  saída
+ou  uma?
+e)printf("%s\n%s\n%s\n", "um", "dois", "três");
+
+um
+dois
+três
