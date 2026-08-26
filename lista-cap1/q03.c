bf90b13663fbc82a2412f392d2d34405c4d7352e
Danilo Barros <barrosnilo2@gmail.com>
2026-08-26 14:44:53 -0300

    Create q03.c

---
+++ lista-cap1/q03.c
@@ -0,0 +1,17 @@
+/* 
+ * Esse programa mostra o uso de comentários em várias linhas
+ * e mostra também o uso de comentários em uma única linha.
+ * Primeiro programa
+ */
+
+#include <stdio.h>  /* Para printf() */
+#include <stdlib.h> /* Para system() */
+
+int main(void) /* Função main */
+{ /* Início do corpo da função main */
+    // Exibe a mensagem do programa na tela (comentário de linha única)
+    printf("Primeiro programa."); /* Chamada à função printf */
+    
+    system("PAUSE"); /* Chamada à função system */
+    return 0;
+} /* Fim do corpo da função main */
